6fa7bf86b0a6802399cde6be7ffc4e274a067dcf
Milemotigo <mtigo@gmail.com>
2023-06-07 09:55:07 -0700

    model

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,9 @@ class BaseModel:
         self.created_at = created_at
         self.updated_at = updated_at
     def __str__(self):
-        return (f"UUID: {self.uuid}, created_at: {self.created_at}, updated_at: {self.updated_at}")
+        return (f" UUID: {self.uuid}, created_at: {self.created_at}, updated_at: {self.updated_at}")
 my_object = BaseModel("2023-6-7", "2023-6-7")
 print(f" User id is {my_object.uuid}")
 print(f" Created the account in {my_object.updated_at}")
 print(f" Updated the account in {my_object.created_at}")
-print(my_object)
+print( my_object)
