82dfb5e57c900e1fcca0b3fe0aa101ad632cd03b
Milemotigo <mtigo@gmail.com>
2023-06-07 09:51:11 -0700

    model

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,15 @@
 import uuid
 class BaseModel:
-    def __init__(self):
+    def __init__(self, created_at, updated_at):
         print("i just created an object")
         #Every object the we are going to be creating will have the following attributes
         self.uuid = uuid.uuid4()
         self.created_at = created_at
         self.updated_at = updated_at
     def __str__(self):
-        return (f"UUID: {self.uuid.uuid4}, created_at: {self.created_at}, updated_at: {self.updated_at}")
-print(str(BaseModel()))
+        return (f"UUID: {self.uuid}, created_at: {self.created_at}, updated_at: {self.updated_at}")
 my_object = BaseModel("2023-6-7", "2023-6-7")
 print(f" User id is {my_object.uuid}")
 print(f" Created the account in {my_object.updated_at}")
-print(f"Updated the account in {my_object.created}")
+print(f" Updated the account in {my_object.created_at}")
 print(my_object)
