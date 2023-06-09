0bd213f553b8db955c0c99ef40b186d407787bb1
Milemotigo <mtigo@gmail.com>
2023-06-08 20:12:20 -0700

    new_starting

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,31 @@
 import uuid
+from datetime import datetime
 class BaseModel:
-    def __init__(self, created_at, updated_at):
-        print("i just created an object")
+    def __init__(self):
+        #print("i just created an object")
         #Every object the we are going to be creating will have the following attributes
-        self.uuid = uuid.uuid4()
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.id = str(uuid.uuid4())
+        # 
+        self.created_at = datetime.fromisoformat('2023-06-08T17:16:59.005586')
+        self.updated_at = datetime.fromisoformat('2023-06-08T17:16:59.005586')
     def __str__(self):
-        return (f" UUID: {self.uuid}, created_at: {self.created_at}, updated_at: {self.updated_at}")
-my_object = BaseModel("2023-6-7", "2023-6-7")
-print(f" User id is {my_object.uuid}")
-print(f" Created the account in {my_object.updated_at}")
-print(f" Updated the account in {my_object.created_at}")
-print( my_object)
+        str_dict = f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
+        return (str_dict)
+    def save(self):
+        self.updated_at = datetime.now().isoformat()
+        return self.updated_at
+        #self.created_at = datetime.datetime.now().isoformat()
+    def to_dict(self):
+        to_dict = {}
+        to_dict['__class__'] = self.__class__.__name__
+        to_dict.update(self.__dict__)
+        return to_dict
+#my_object = BaseModel()
+#print(f" User id is {my_object.uuid}")
+#print(f" Created the account in {my_object.updated_at}")
+#print(f" Updated the account in {my_object.created_at}")
+#print( my_object.__str__())
+#my_object.save()
+#print(f" save method {my_object.updated_at}")
+#my_object.to_dict()
+#print(my_object.to_dict)
